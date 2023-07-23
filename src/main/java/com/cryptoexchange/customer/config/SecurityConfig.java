b8a80be1312aae861e3c05d91838059a8bf916f6
Nikita Bolshakov <bnikmik@gmail.com>
2023-07-23 13:09:11 +0300

    spring security + keycloak + docker-compose

---
+++ src/main/java/com/cryptoexchange/customer/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cryptoexchange.customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+}
